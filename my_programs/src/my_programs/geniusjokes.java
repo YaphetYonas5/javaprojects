d101625b34dfe708da3efe9969a87ad63523469f
HGP53 <hgp53@Thes-MacBook-Pro.local>
2021-09-27 18:11:34 -0700

    creating our main java folder structure

---
+++ my_programs/src/my_programs/geniusjokes.java
@@ -0,0 +1,9 @@
+package my_programs;
+
+public class geniusjokes {
+	 public static void main(String[] args) {
+		    System.out.println("Hello my name is yaphet"); //print name and welcome message
+		    System.out.println("What do you call a pig that does karate?"); //print joke setup
+		    System.out.println("a pork chop"); //print punch line
+		  }
+}
