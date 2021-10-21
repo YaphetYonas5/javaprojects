e08f5fef0a6b7df7f68b0fbcc03740c9309c5033
HGP53 <hgp53@Thes-MacBook-Pro.local>
2021-10-20 17:42:24 -0700

    created new branch called genius and add genius class program

---
+++ my_programs/src/my_programs/Genius.java
@@ -0,0 +1,126 @@
+package my_programs;
+
+public class Genius {
+	
+	
+
+		 
+	public static void main(String[] args) { 
+		gotnames();
+		gotJokes1();
+		gotJokes2();
+		gotJokes3();
+		gotJokes4();
+		gotCommunityNorms();
+		gotsports();
+		gotmusic();
+		gotschools();
+		gotage();
+	
+	}
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+		 
+		 public static void gotnames() {
+		 
+			 
+		 	 System.out.println("1. My Name Is Yaphet Yonas.");  //Print Genius Name
+			 System.out.println("2. My Name Is Brendan Kariithi.");  //Print Genius Name
+			 System.out.println("3. My Name Is Omari Pratt.");  //Print Genius Name
+			 System.out.println("4. My Name Is Isaiah Wilson.");  //Print Genius Name
+		 }
+			 
+		 
+	 
+	 /*gotJokes Method Declaration */
+	 public static void gotJokes1() {
+		 
+		 /* GENIUS JOKES - WE GOT JOKES */	
+        System.out.println("Welcome, My Name Is Yaphet!");  //Print My Name
+        System.out.println("1. what do you call a pig that does karate?");  //Joke Setup 
+        System.out.println("a pork chop");  //Joke Punch Line
+        		 
+	 } 
+	 
+	 /*gotJokes Method Declaration */
+	 public static void gotJokes4() {
+		 
+		 /* GENIUS JOKES - WE GOT JOKES */	
+        System.out.println("4. why did the chicken cross the road");  //Print My Name
+        System.out.println("to get to the other side");  //Joke Setup 
+	 }
+	 
+	 /*gotJokes Method Declaration */
+	 public static void gotJokes3() {
+		 
+		 /* GENIUS JOKES - WE GOT JOKES */	
+        System.out.println("3. where do fish get their money");  //Print My Name
+        System.out.println("the river bank");  //Joke Setup 
+	 }
+	 
+	 /*gotJokes Method Declaration */
+	 public static void gotJokes2() {
+		 
+		 /* GENIUS JOKES - WE GOT JOKES */	
+        System.out.println("2. what tastes better than toes");  //Print My Name
+        System.out.println("chicken does");  //Joke Setup 
+	 }
+	 
+	 /*gotCommunityNorms Method Declaration */
+	 public static void gotCommunityNorms() {
+		/* GENIUS COMMUNITY NORMS - KEEP US ACCOUNTABLE */
+		System.out.println("OUR COMMUNITY NORMS");
+		System.out.println("----------------------");
+		System.out.println("1. Everyone should participate equaly");
+		System.out.println("2. Everyone should be doing their fare share of work in group projects");
+		System.out.println("3. We should all give our 100% and take class seriously");
+		System.out.println("4. We should not give up and stick with it");
+		System.out.println("----------------------");
+		System.out.println("");  //Extra Line
+        System.out.println("");  //Extra Line
+			 
+	 }
+	 
+	 /*gotSport Method Declaration */
+	 public static void gotsports() {
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+			 System.out.println("1. soccer.");  //Print Genius Name
+			 System.out.println("2. basketball.");  //Print Genius Name
+			 System.out.println("3. lacrosse.");  //Print Genius Name
+			 System.out.println("4. basketball.");  //Print Genius Name
+	
+		 }
+	 
+	 /*gotSport Method Declaration */
+	 public static void gotmusic() {
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+			 System.out.println("1. HipHop.");  //Print Genius Name
+			 System.out.println("2. Really love.");  //Print Genius Name
+			 System.out.println("3. rap.");  //Print Genius Name
+			 System.out.println("4. Already best friends.");  //Print Genius Name
+			 
+	 }
+	 
+	 /*gotSchools Method Declaration */
+	 public static void gotschools() {
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+			 System.out.println("1. salesian.");  //Print Genius Name
+			 System.out.println("2. middle colledge.");  //Print Genius Name
+			 System.out.println("3. albany highschool.");  //Print Genius Name
+			 System.out.println("4. Colledge prep");  //Print Genius Name
+			 
+		 }
+	 /*gotAge Method Declaration */
+	 public static void gotage() {
+			 
+			 /* GENIUS NAMES - SAY OUR NAMES */
+			 System.out.println("1. 14.");  //Print Genius Name
+			 System.out.println("2. 16.");  //Print Genius Name
+			 System.out.println("3. 14.");  //Print Genius Name
+			 System.out.println("4. 15.");  //Print Genius Name
+			 
+		 }
+}
